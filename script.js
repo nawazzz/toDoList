cd8c8c8fecbfb5fc33b8d14485c34f384fd54b01
nawazzz <65383832+nawazzz@users.noreply.github.com>
2021-01-28 18:25:34 +0530

    create toDolist

---
+++ script.js
@@ -0,0 +1,99 @@
+// create h1 element for heading "todos"
+let h1 = document.createElement("h1")
+h1.textContent = "todos"
+document.body.appendChild(h1)
+
+// create a div element
+let div = document.createElement("div")
+div.classList.add("parentOfInput")
+document.body.appendChild(div)
+
+// create ul
+let ul = document.createElement("ul")
+document.body.appendChild(ul)
+ul.classList.add("ulStyling")
+
+// create input element for the search box and append it in the div element as child
+let input = document.createElement("input")
+input.placeholder = "What needs to be done?"
+div.appendChild(input)
+// add class and attribute to the input element for styling
+input.classList.add("searchBox")
+input.setAttribute("autofocus", "")
+
+
+
+// create an array to save the input values
+allTodo = []
+
+// function for appending list upon hitting enter after entering text
+function createListElement(event) {
+    let randomNumber = Math.floor(Math.random()*90000) + 10000
+    let li = document.createElement("li")
+    li.classList.add("listStyling")
+    li.setAttribute("id", randomNumber)
+    let radioButton = document.createElement("input")  // create radio button 
+    radioButton.setAttribute("type", "radio")
+    let closeButton = document.createElement("span")  // create close buttom in list
+    closeButton.textContent = "x"
+    closeButton.classList.add("closeButton")
+    if (event.keyCode === 13) {
+        let obj = {
+            text: input.value,
+            isComplicated: false,
+            id: randomNumber
+        }
+        allTodo.push(obj)
+        for (let i = 0; i < allTodo.length; i++) {
+            li.textContent = allTodo[i].text
+        }
+        ul.appendChild(li)
+        li.appendChild(radioButton)
+        li.appendChild(closeButton)
+        // input.value = ""               
+    }
+}
+
+function deleteListElement(event) {
+        let li = document.createElement("li")
+        li.classList.add("listStyling") 
+        let radioButton = document.createElement("input")  // create radio button 
+        radioButton.setAttribute("type", "radio")
+        let closeButton = document.createElement("span")  // create close buttom in list
+        closeButton.textContent = "x"
+        closeButton.classList.add("closeButton")
+    // console.log(event)
+        allTodo = allTodo.filter(function(element, index){
+        if (element.id !== Number(event.target.parentNode.id)) {
+            return true;
+        }
+    })
+        document.querySelector("ul").textContent = "";
+
+        console.log(allTodo)
+
+        // for (let i = 0; i < allTodo.length; i++) {
+        //     li.textContent = allTodo[i].text
+        //     li.setAttribute("id", allTodo[i].id)
+        //     console.log(li)
+        //     li.appendChild(radioButton)
+        //     li.appendChild(closeButton)
+        //     ul.appendChild(li)
+        // }
+
+        allTodo.forEach((element, index) => {
+          return document.querySelector("ul").innerHTML += `
+            <li class="listStyling" id="${element.id}"">${element.text}
+              <input type="radio">
+              <span class="closeButton">x</span>
+            </li>
+          `
+        })
+
+            
+
+}
+
+input.addEventListener("keypress", createListElement)
+
+ul.addEventListener("click", deleteListElement)
